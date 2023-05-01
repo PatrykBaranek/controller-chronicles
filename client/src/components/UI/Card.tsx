630448aa770270dc03191ae73f9ca9aa8a9f540a
mbartos <maciekbartos8@gmail.com>
2023-05-01 12:25:52 +0200

    Fix gradient border

--- client/src/components/UI/Card.tsx
+++ client/src/components/UI/Card.tsx
@@ -4,9 +4,27 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   overflow: hidden;
-  background: ${({ theme }) => theme.colors.cardGradient};
-  border-radius: 1rem;
+  /* background: ${({ theme }) => theme.colors.cardGradient};
+  border-radius: 1rem; */
+  position: relative;
   padding: 1px;
+  &::before {
+    content: '';
+    position: absolute;
+    inset: 0;
+    border-radius: 1rem;
+    padding: 1px;
+    background: linear-gradient(
+      180deg,
+      rgba(255, 255, 255, 0.7) 0%,
+      rgba(255, 255, 255, 0.6) 30%,
+      rgba(255, 255, 255, 0) 100%
+    );
+    mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+    mask-composite: xor;
+    mask-composite: exclude;
+    pointer-events: none;
+  }
 `;
 const StyledCard = styled.div`
   display: flex;
