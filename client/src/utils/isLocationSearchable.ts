6264c38795868da7fd3b0654df3fb93f5c0e9b1b
mbartos <maciekbartos8@gmail.com>
2023-05-03 12:28:19 +0200

    Add utility functions

---
+++ client/src/utils/isLocationSearchable.ts
@@ -0,0 +1,7 @@
+const isLocationSearchable = (location: string): boolean => {
+  return (
+    location.slice(0, 6) === '/games' || location.slice(0, 9) === '/podcasts'
+  );
+};
+
+export default isLocationSearchable;
