7443fa40f126e2f8f4b7a9930521981869d0791a
patrykbaranek <64849496+PatrykBaranek@users.noreply.github.com>
2023-05-03 22:28:01 +0200

    add mongoose and youtube module

--- server/src/games/games.module.ts
+++ server/src/games/games.module.ts
@@ -1,12 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { GamesController } from './games.controller';
 import { GamesService } from './games.service';
 import { HttpModule } from '@nestjs/axios';
-import { YoutubeService } from './youtube/youtube.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Game, GameSchema } from './models/Game.schema';
+import { GamesRepository } from './games.repository';
+import {
+  GameTrailers,
+  GameTrailersSchema,
+} from './youtube/models/trailers.schema';
+import { YoutubeModule } from './youtube/youtube.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    MongooseModule.forFeature([
+      { name: Game.name, schema: GameSchema },
+      { name: GameTrailers.name, schema: GameTrailersSchema },
+    ]),
+    forwardRef(() => YoutubeModule),
+  ],
   controllers: [GamesController],
-  providers: [GamesService, YoutubeService],
+  providers: [GamesService, GamesRepository],
+  exports: [GamesService],
 })
 export class GamesModule {}
