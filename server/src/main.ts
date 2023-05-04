9caea4e227f7cf45e05131a8b59438d7ddd63278
patrykbaranek <64849496+PatrykBaranek@users.noreply.github.com>
2023-05-05 00:05:10 +0200

    add swagger documentation for spotify controllers

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { SpotifyAuthService } from './spotify/spotify-auth/spotify-auth.service';
+import scopes from './spotify/scopes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,11 +11,24 @@ async function bootstrap() {
   });
   app.useGlobalPipes(new ValidationPipe());
 
+  const spotifyAuthService = app.get(SpotifyAuthService);
+
   const config = new DocumentBuilder()
     .setTitle('Controllers Chronicles Api')
     .setDescription('The Controllers Chronicles API description')
     .setVersion('1.0')
-    .addTag('controllers-chronicles')
+    .addOAuth2({
+      type: 'oauth2',
+      flows: {
+        authorizationCode: {
+          authorizationUrl: spotifyAuthService.getAuthorizeURL(),
+          tokenUrl: 'http://localhost:3000/spotify/auth/token',
+          scopes: {
+            ...scopes,
+          },
+        },
+      },
+    })
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
--- server/src/spotify/dto/find-episode-by-game.dto.ts
+++ server/src/spotify/dto/find-episode-by-game.dto.ts
@@ -5,20 +5,37 @@ import {
   IsUppercase,
   MaxLength,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class FindEpisodeByGameDto {
+  @ApiProperty({ description: 'The game title', type: String })
   @IsNotEmpty()
   @IsString()
   gameTitle: string;
 
+  @ApiProperty({
+    description: 'The limit of results',
+    minimum: 10,
+    type: Number,
+  })
   @IsNumber()
   limit: number;
 
+  @ApiProperty({
+    description: 'The language code',
+    maxLength: 2,
+    enum: ['pl', 'en'],
+  })
   @IsNotEmpty()
   @IsString()
   @MaxLength(2)
   language: string;
 
+  @ApiProperty({
+    description: 'The market code',
+    maxLength: 2,
+    enum: ['PL', 'EN'],
+  })
   @IsUppercase()
   @IsNotEmpty()
   @MaxLength(2)
