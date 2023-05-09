e2de4e6dd5ee7e76b73269149182bb67b046d66f
mbartos <maciekbartos8@gmail.com>
2023-05-09 13:49:00 +0200

    Add games card with data fetching and games page with pagination

--- client/src/components/GameCard/GameCard.tsx
+++ client/src/components/GameCard/GameCard.tsx
@@ -1,6 +1,11 @@
 import { Gamecard } from '#/types/types';
 import styled from 'styled-components';
 import Card from '../UI/Card';
+import { Link } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { getGameById } from '#/api/gamesApi';
+import { Skeleton } from '@mui/material';
+
 const StyledImage = styled.div`
   width: 100%;
   height: 50%;
@@ -14,56 +19,126 @@ const StyledContent = styled.div`
   width: 100%;
   height: 50%;
   padding-inline: 1rem;
-  padding-top: 1.5rem;
+  padding-top: 1.2rem;
 `;
 const StyledTopSection = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   h1 {
+    width: 100%;
+    color: white;
     font-weight: ${({ theme }) => theme.fontWeights.bold};
-    font-size: 1rem;
+    font-size: clamp(0.8rem, 2vw, 1rem);
+    @media screen and (min-width: 900px) {
+      font-size: clamp(0.7rem, 1vw, 1rem);
+    }
   }
   p {
+    width: 100%;
+    text-align: right;
+    font-size: clamp(0.8rem, 2vw, 1rem);
     color: ${({ theme }) => theme.colors.primary};
+    @media screen and (min-width: 900px) {
+      font-size: clamp(0.7rem, 1vw, 1rem);
+    }
     span {
+      display: inline-block;
+      margin-top: 5px;
       color: ${({ theme }) => theme.colors.yellow};
     }
   }
 `;
 const StyledDescription = styled.div`
   color: ${({ theme }) => theme.colors.primary};
-  margin-block: 1rem;
+  margin-block: 0.3rem;
   line-height: 1.1;
   p {
     font-size: 0.8rem;
   }
 `;
 
-const GameCard = ({ image, title, description, rating }: Gamecard) => {
+const GameCard = ({ id, image, title, rating }: Gamecard) => {
+  const { isLoading, data } = useQuery(['/games/:id', id], () =>
+    getGameById(id)
+  );
+  const description = data && data.rawgGame.description_raw;
   return (
     <Card>
-      <StyledImage>
-        <img
-          src={image}
-          alt=''
-        />
-      </StyledImage>
-      <StyledContent>
-        <StyledTopSection>
-          <h1>Title</h1>
-          <p>
-            Rating <span>4.5/5</span>
-          </p>
-        </StyledTopSection>
-        <StyledDescription>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod non
-            dolorem fugiat fuga quisquam. Nam vel perspiciatis quos quam libero
-            fugit necessitatibus, odio saepe veritatis eos cupiditate
-            repellendus itaque tempora!
-          </p>
-        </StyledDescription>
-      </StyledContent>
+      <Link to={`${id}`}>
+        <StyledImage>
+          {!isLoading && image ? (
+            <img
+              src={image}
+              alt={`${title} image`}
+            />
+          ) : (
+            <Skeleton
+              sx={{
+                minHeight: '154px',
+                backgroundImage:
+                  'linear-gradient(131.88deg, #a63ee73b 14.48%, #00eaff2d 83.43%)',
+                borderRadius: '1rem 1rem 0 0 ',
+              }}
+              animation='wave'
+              variant='rounded'
+              height={'100%'}
+            />
+          )}
+        </StyledImage>
+
+        <StyledContent>
+          <StyledTopSection>
+            {!isLoading && title ? (
+              <h1>{title}</h1>
+            ) : (
+              <Skeleton
+                sx={{
+                  backgroundImage:
+                    'linear-gradient(131.88deg, #a63ee73b 14.48%, #00eaff2d 83.43%)',
+                  marginRight: '10px',
+                }}
+                animation='wave'
+                variant='rounded'
+              />
+            )}
+            {!isLoading ? (
+              <p>
+                Rating <span>{rating ? rating : 0}/10</span>
+              </p>
+            ) : (
+              <Skeleton
+                sx={{
+                  backgroundImage:
+                    'linear-gradient(131.88deg, #a63ee73b 14.48%, #00eaff2d 83.43%)',
+                }}
+                animation='wave'
+                variant='rounded'
+              >
+                <p>
+                  Rating <span>{rating}/10</span>
+                </p>
+              </Skeleton>
+            )}
+          </StyledTopSection>
+          <StyledDescription>
+            {!isLoading && description ? (
+              <p>{description && description.slice(0, 250) + ' ...'}</p>
+            ) : (
+              <Skeleton
+                sx={{
+                  marginBottom: '10px',
+                  backgroundImage:
+                    'linear-gradient(131.88deg, #a63ee73b 14.48%, #00eaff2d 83.43%)',
+                }}
+                animation='wave'
+                height={'100px'}
+                variant='rounded'
+              />
+            )}
+          </StyledDescription>
+        </StyledContent>
+      </Link>
     </Card>
   );
 };
--- client/src/pages/Games.tsx
+++ client/src/pages/Games.tsx
@@ -1,13 +1,135 @@
-import { Pagination } from '@mui/material';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Pagination, ThemeProvider, createTheme } from '@mui/material';
+import { useQuery } from 'react-query';
+import { getGames } from '#/api/gamesApi';
+import GameCard from '#/components/GameCard/GameCard';
+import useWindowWidth from '#/hooks/useWindowWidth';
+import isDesktopWidth from '#/utils/isDesktopWidth';
+const StyledContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const StyledWrapper = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+  row-gap: 2rem;
+  @media screen and (min-width: 500px) {
+    padding-top: 2rem;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1.5rem;
+  }
+  @media screen and (min-width: 900px) {
+    padding-top: 2rem;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1.5rem;
+  }
+  @media screen and (min-width: 1050px) {
+    padding-inline: 2rem;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(2, 23rem);
+  }
+`;
 
 const Games = () => {
+  const windowWidth = useWindowWidth();
+  const isDesktop = isDesktopWidth(windowWidth);
+  const [page, setPage] = useState(1);
+  const { isLoading, isError, error, data } = useQuery(
+    ['/games', page],
+    () => getGames(page),
+    {
+      keepPreviousData: true,
+    }
+  );
+
+  useEffect(() => {
+    data && setPage(data.currentPage);
+  }, [data]);
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPage(value);
+  };
+
+  const theme = createTheme({
+    components: {
+      MuiPagination: {
+        styleOverrides: {
+          root: {
+            display: 'flex',
+            justifyContent: 'center',
+            marginBlock: '1rem',
+          },
+          outlined: {
+            button: {
+              fontFamily: 'Inter',
+              fontSize: '.8rem',
+              background: 'transparent',
+              color: 'white',
+              borderColor: 'rgba(235, 235, 245, 0.2)',
+              '&:hover': {
+                background: `linear-gradient(131.88deg, rgba(167, 62, 231, 0.15) 14.48%, rgba(0, 235, 255, 0.15) 83.43%)`,
+              },
+            },
+          },
+          ul: {
+            gap: '.2rem',
+          },
+        },
+      },
+      MuiPaginationItem: {
+        styleOverrides: {
+          root: {
+            color: 'white',
+
+            '&.Mui-selected': {
+              background: `linear-gradient(131.88deg, rgba(167, 62, 231, 0.15) 14.48%, rgba(0, 235, 255, 0.15) 83.43%)`,
+            },
+          },
+
+          sizeSmall: {
+            borderRadius: '14px',
+            margin: '0 2px',
+            padding: '0 5px',
+            minWidth: '28px',
+            height: '28px',
+          },
+        },
+      },
+    },
+  });
+
   return (
-    <div>
-      <Pagination
-        count={10}
-        variant='outlined'
-      />
-    </div>
+    <StyledContainer>
+      <StyledWrapper>
+        {data?.results.map(game => (
+          <GameCard
+            key={game.id}
+            id={game.id}
+            title={game.name}
+            image={game.background_image}
+            rating={game.metacritic / 10}
+          />
+        ))}
+      </StyledWrapper>
+      <ThemeProvider theme={theme}>
+        <Pagination
+          siblingCount={isDesktop ? 1 : 0}
+          size={isDesktop ? 'medium' : 'small'}
+          count={data?.totalPages}
+          variant='outlined'
+          page={page}
+          onChange={handlePageChange}
+        />
+      </ThemeProvider>
+    </StyledContainer>
   );
 };
 
