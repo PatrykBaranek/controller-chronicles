4e5d1c6f1f1eb4503f284dfdc7cb586c7ad8ac4d
patrykbaranek <64849496+PatrykBaranek@users.noreply.github.com>
2023-05-10 20:36:57 +0200

    add cron functionality and separate logic

--- server/src/games/steam/steam.service.ts
+++ server/src/games/steam/steam.service.ts
@@ -1,21 +1,40 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { PuppeteerService } from '../puppeteer/puppeteer.service';
 import { SteamReposiiory } from './steam.repository';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class SteamService {
+  private readonly logger = new Logger(SteamService.name);
+
   constructor(
     private readonly steamRepository: SteamReposiiory,
     private readonly puppeteerService: PuppeteerService,
   ) {}
 
-  async getBestSellers(cc: 'pl' | 'us') {
+  async getBestSellers() {
+    if (await this.checkIfTodaysBestSellersExist()) {
+      this.logger.log('Getting bestsellers from DB');
+      return this.steamRepository.getBestSellers();
+    }
+
+    const bestSellersJson = await this.scrapeBestSellers();
+
+    return bestSellersJson;
+  }
+
+  @Cron(CronExpression.EVERY_12_HOURS)
+  private async scrapeBestSellers() {
+    this.logger.log('Scraping bestsellers from Steam');
     const browser = await this.puppeteerService.launchBrowser();
     try {
       const page = await this.puppeteerService.createPage(
         browser,
-        'https://store.steampowered.com/search/?category1=998&os=win%2Cmac%2Clinux&specials=1&hidef2p=1&filter=topsellers&ndl=1&cc=' +
-          cc,
+        'https://store.steampowered.com/search/?category1=998&os=win%2Cmac%2Clinux&specials=1&hidef2p=1&filter=topsellers&ndl=1&ignore_preferences=1',
       );
 
       const bestsellersList = await page.waitForSelector('#search_resultsRows');
@@ -48,25 +67,22 @@ export class SteamService {
             }),
           );
 
-          const bestsellerToSave = {
+          return {
             img,
             name,
             link,
             price,
           };
-
-          await this.steamRepository.saveBestSellers({
-            games: bestsellerToSave,
-            updateDate: new Date(),
-          });
-
-          return bestsellerToSave;
         }),
       );
 
+      await this.steamRepository.saveBestSellers({
+        games: bestSellersJson,
+      });
+
       return bestSellersJson;
     } catch (err) {
-      throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new InternalServerErrorException(err);
     } finally {
       await this.puppeteerService.closeBrowser(browser);
     }
