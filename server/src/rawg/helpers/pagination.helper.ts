9fedb4318c1553642596831b09219b13ab331118
patrykbaranek <64849496+PatrykBaranek@users.noreply.github.com>
2023-05-14 22:36:11 +0200

    add helper function to rawg-api module

---
+++ server/src/rawg/helpers/pagination.helper.ts
@@ -0,0 +1,37 @@
+import { AxiosResponse } from 'axios';
+import { PaginationDto } from './dto/pagination.dto';
+import { NotFoundException } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+
+export function paginateResponse<T>(
+  response: AxiosResponse,
+  page: number = 1,
+  page_size: number = 5,
+  returnType: new () => T,
+  options: { showTotalPages?: boolean },
+): PaginationDto<T> {
+  const totalPages = Math.ceil(response.data.count / page_size);
+
+  if (page > totalPages) {
+    throw new NotFoundException('Page number cannot be greater than page size');
+  }
+
+  const results = response.data.results.map((result) =>
+    plainToInstance(returnType, result),
+  );
+
+  if (!options?.showTotalPages) {
+    return {
+      totalItems: response.data.count,
+      currentPage: page,
+      results,
+    };
+  }
+
+  return {
+    totalItems: response.data.count,
+    totalPages,
+    currentPage: page,
+    results,
+  };
+}
