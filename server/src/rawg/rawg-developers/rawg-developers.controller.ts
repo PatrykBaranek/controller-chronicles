d9f02e0b7f86f8e7767791640ca7bb3fefa42ed9
Patryk Baranek <cien2460045@gmail.com>
2023-05-20 11:21:01 +0200

    add swagger tags and operations

--- server/src/rawg/rawg-developers/rawg-developers.controller.ts
+++ server/src/rawg/rawg-developers/rawg-developers.controller.ts
@@ -1,10 +1,19 @@
 import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { RawgDevelopersService } from './rawg-developers.service';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { RawgDeveloperResponseDto } from './dto/rawg-developer-response.dto';
 
+@ApiTags('developers')
 @Controller('developers')
 export class RawgDevelopersController {
   constructor(private readonly developersService: RawgDevelopersService) {}
 
+  @ApiOperation({ summary: 'Get all developers' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return all developers',
+    type: RawgDeveloperResponseDto,
+  })
   @Get()
   getDevelopers(
     @Query('page') page: number,
@@ -13,6 +22,8 @@ export class RawgDevelopersController {
     return this.developersService.getDevelopers(page, page_size);
   }
 
+  @ApiOperation({ summary: 'Get developer by id' })
+  @ApiParam({ name: 'id', type: Number })
   @Get('/:id')
   getDeveloperById(@Param('id', ParseIntPipe) id: number) {
     return this.developersService.getDeveloper(id);
--- server/src/rawg/rawg-tags/rawg-tags.controller.ts
+++ server/src/rawg/rawg-tags/rawg-tags.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
 import { RawgTagsService } from './rawg-tags.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('tags')
 @Controller('tags')
 export class RawgTagsController {
   constructor(private readonly tagsService: RawgTagsService) {}
 
+  @ApiOperation({ summary: 'Get all tags from rawgapi' })
   @Get()
   async getTags(
     @Query('page', ParseIntPipe) page: number,
