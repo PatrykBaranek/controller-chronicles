13d5babfa04bea3ab33fa99d0edcbdbce78bbace
patrykbaranek <64849496+PatrykBaranek@users.noreply.github.com>
2023-05-21 18:52:06 +0200

    tba review sites

--- server/src/reviews-sites/reviews-sites-gram-pl/reviews-sites-gram-pl.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ReviewsSitesGramPlService } from './reviews-sites-gram-pl.service';
-import { PuppeteerModule } from 'src/puppeteer/puppeteer.module';
-
-@Module({
-  imports: [PuppeteerModule],
-  providers: [ReviewsSitesGramPlService],
-})
-export class ReviewsSitesGramPlModule {}
--- server/src/reviews-sites/reviews-sites-ign/reviews-sites-ign.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ReviewsSitesIgnService } from './reviews-sites-ign.service';
-import { PuppeteerModule } from 'src/puppeteer/puppeteer.module';
-
-@Module({
-  imports: [PuppeteerModule],
-  providers: [ReviewsSitesIgnService],
-})
-export class ReviewsSitesIgnModule {}
