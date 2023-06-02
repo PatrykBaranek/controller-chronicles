78bf8de2fe2e7e72bcfaba7ac95ea2917779b0b6
patrykbaranek <64849496+PatrykBaranek@users.noreply.github.com>
2023-06-03 00:40:31 +0200

    added swagger to other routes

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -13,17 +13,34 @@ import { CreateUserDto } from './users/dto/create-user.dto';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { Request, Response } from 'express';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiTags,
+  ApiResponse,
+  ApiCookieAuth,
+} from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Create user' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully created.',
+  })
   @Post('/signup')
   async createUser(@Body() createUserDto: CreateUserDto) {
     return this.authService.register(createUserDto);
   }
 
   @UseGuards(LocalAuthGuard)
+  @ApiOperation({ summary: 'Log in' })
+  @ApiBody({ description: 'User credentials', type: CreateUserDto })
+  @ApiResponse({ status: 200, description: 'Logged in successfully' })
   @Post('/login')
   async login(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
     const { access_token } = await this.authService.login(req.user);
@@ -41,11 +58,16 @@ export class AuthController {
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Get profile' })
+  @ApiResponse({ status: 200, description: 'Returned user profile' })
+  @ApiCookieAuth('access_token')
   @Get('/profile')
   async getProfile(@Req() req) {
     return req.user;
   }
 
+  @ApiOperation({ summary: 'Log out' })
+  @ApiResponse({ status: 200, description: 'Logged out successfully' })
   @Get('/logout')
   async logout(@Req() req) {
     req.res.clearCookie('access_token');
--- server/src/collections/collections.controller.ts
+++ server/src/collections/collections.controller.ts
@@ -12,7 +12,7 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { AddGameToCollectionDto } from './dto/add-game-to-collection.dto';
 import { CreateNewCollectionDto } from './dto/create-new-collection.dto';
 import { DeleteCollectionDto } from './dto/delete-collection.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('collections')
 @Controller('collections')
@@ -20,6 +20,9 @@ export class CollectionsController {
   constructor(private collectionsService: CollectionsService) {}
 
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Create a new collection' })
+  @ApiBody({ type: CreateNewCollectionDto })
   @Post('createCollection')
   async createCollection(
     @Req() req,
@@ -32,12 +35,18 @@ export class CollectionsController {
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Delete a collection' })
+  @ApiBody({ type: DeleteCollectionDto })
   @Delete('deleteCollection')
   async deleteCollection(@Body() deleteCollectionDto: DeleteCollectionDto) {
     return this.collectionsService.deleteCollection(deleteCollectionDto);
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Add a game to a collection' })
+  @ApiBody({ type: AddGameToCollectionDto })
   @Post('addGame')
   async addGame(
     @Body() addGameToCollectionDto: AddGameToCollectionDto,
@@ -50,6 +59,8 @@ export class CollectionsController {
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get all collections of a user' })
   @Get('getCollections')
   async getCollections(@Req() req) {
     return this.collectionsService.getCollections(req.user.userId);
