e5df91deda745359429173eef8d4416d22a21664
patrykbaranek <64849496+PatrykBaranek@users.noreply.github.com>
2023-06-03 00:03:22 +0200

    add guard to soundtracks

--- server/src/spotify/spotify-soundtracks/spotify-soundtracks.controller.ts
+++ server/src/spotify/spotify-soundtracks/spotify-soundtracks.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { SpotifySoundtracksService } from './spotify-soundtracks.service';
 import { ApiTags } from '@nestjs/swagger';
+import { SpotifyAuthGuard } from '../guards/spotify-auth.guard';
 
 @ApiTags('spotify/soundtracks')
 @Controller('spotify/soundtracks')
+@UseGuards(SpotifyAuthGuard)
 export class SpotifySoundtracksController {
   constructor(
     private readonly spotifySoundtracksService: SpotifySoundtracksService,
