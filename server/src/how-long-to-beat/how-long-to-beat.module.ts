36e9b0ce31f7c2c3a6fddabcecfb64accbca6577
patrykbaranek <64849496+PatrykBaranek@users.noreply.github.com>
2023-06-24 21:34:51 +0200

    add puppeteer service to hltbmodule

--- server/src/how-long-to-beat/how-long-to-beat.module.ts
+++ server/src/how-long-to-beat/how-long-to-beat.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HowLongToBeatService } from './how-long-to-beat.service';
+import { PuppeteerModule } from 'src/puppeteer/puppeteer.module';
+import { PuppeteerService } from 'src/puppeteer/puppeteer.service';
 
 @Module({
-  providers: [HowLongToBeatService],
-  exports: [HowLongToBeatService],
+  providers: [HowLongToBeatService, PuppeteerService],
+  imports: [PuppeteerModule],
+  exports: [HowLongToBeatService, PuppeteerService],
 })
 export class HowLongToBeatModule {}
