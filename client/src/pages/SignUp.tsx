bf845ebf5734af7fbfcfcfcfe68e46bee24e5f80
mbartos <maciekbartos8@gmail.com>
2023-08-05 12:41:15 +0200

    Add alerts

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -14,31 +14,65 @@ import {
 import eye from '#/assets/eye.svg';
 import crossedEye from '#/assets/crossedEye.svg';
 import { validateEmail, validatePassword } from '#/utils/formValidation';
-type Inputs = {
-  email: string;
-  password: string;
-};
+import { useMutation } from 'react-query';
+import { signUpUser } from '#/api/gamesApi';
+import { Alert } from '@mui/material';
+import { AuthError, UserInputs } from '#/types/types';
 
 const SignUp = () => {
+  const signUp = useMutation({
+    mutationFn: (data: UserInputs) => signUpUser(data),
+  });
   const [isPasswordShown, setIsPasswordShown] = useState(false);
+  const [isUserRegistered, setIsUserRegistered] = useState(false);
+  const [error, setError] = useState<AuthError>();
   const {
     register,
+    reset,
     handleSubmit,
     formState: { errors },
-  } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = data => {
-    console.log(data);
+  } = useForm<UserInputs>();
+
+  const onSubmit: SubmitHandler<UserInputs> = async data => {
+    signUp.mutate(data, {
+      onSuccess: () => {
+        setIsUserRegistered(true);
+        reset();
+      },
+      onError: (error: any) => {
+        setError(error);
+      },
+    });
   };
 
   return (
     <StyledAuth>
       <StyledAuthWrapper>
+        {isUserRegistered && (
+          <Alert
+            variant='outlined'
+            severity='success'
+            sx={{ color: '#ebebf5bf', marginBottom: '1rem' }}
+          >
+            Hi, your account has been created successfully. You can log in now.
+          </Alert>
+        )}
+        {error && (
+          <Alert
+            variant='outlined'
+            severity='error'
+            sx={{ color: '#ebebf5bf', marginBottom: '1rem' }}
+          >
+            {error.message}
+          </Alert>
+        )}
+
         <Form onSubmit={handleSubmit(onSubmit)}>
           <StyledEmailContainer>
             <StyledInput
               type='text'
               defaultValue=''
-              aria-invalid={errors.email ? true : false}
+              aria-invalid={errors.email || error ? true : false}
               placeholder='Email'
               {...register('email', {
                 validate: v => validateEmail(v),
@@ -50,7 +84,7 @@ const SignUp = () => {
             <StyledInput
               type={isPasswordShown ? 'text' : 'password'}
               defaultValue=''
-              aria-invalid={errors.password ? true : false}
+              aria-invalid={errors.password || error ? true : false}
               placeholder='Password'
               {...register('password', {
                 validate: v => validatePassword(v),
