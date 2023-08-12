891307dc26d70fe045fb51b1e48002f2d2bd1309
mbartos <maciekbartos8@gmail.com>
2023-08-12 14:56:51 +0200

    Fix bestsellers

--- client/src/components/Bestsellers/Bestsellers.tsx
+++ client/src/components/Bestsellers/Bestsellers.tsx
@@ -9,86 +9,91 @@ import { Bestseller } from '#/types/types';
 import { useState } from 'react';
 
 const StyledBestsellers = styled.div`
-  padding-inline: 1rem;
-  margin-top: 10vw;
-  margin-bottom: 1rem;
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  @media screen and (min-width: 900px) {
-    text-align: left;
-    margin-top: 5vw;
-  }
-  h3 {
-    font-size: 1.2rem;
-    font-weight: ${({ theme }) => theme.fontWeights.semiBold};
-    margin-bottom: 1rem;
-  }
+	padding-inline: 1rem;
+	margin-top: 10vw;
+	margin-bottom: 1rem;
+	display: flex;
+	flex-direction: column;
+	text-align: center;
+	@media screen and (min-width: 900px) {
+		text-align: left;
+		margin-top: 5vw;
+	}
+	h3 {
+		font-size: 1.2rem;
+		font-weight: ${({ theme }) => theme.fontWeights.semiBold};
+		margin-bottom: 1rem;
+	}
 `;
-
+const StyledSplideSlide = styled(SplideSlide)`
+	@media screen and (min-width: 900px) {
+		padding: 1.1rem;
+		padding-right: 0;
+	}
+`;
 const Bestsellers = () => {
-  const [bestsellers, setBestsellers] = useState<Bestseller[]>();
-  const { data, isLoading, isError } = useQuery(
-    ['bestsellers'],
-    () => getBestsellers(),
-    {
-      onSuccess: data => setBestsellers(data.slice(0, 10)),
-    }
-  );
+	const [bestsellers, setBestsellers] = useState<Bestseller[]>();
+	const { data, isLoading, isError } = useQuery(
+		['bestsellers'],
+		() => getBestsellers(),
+		{
+			onSuccess: (data) => setBestsellers(data.slice(0, 10)),
+		}
+	);
 
-  return (
-    <StyledBestsellers>
-      <h3>Bestsellers</h3>
-      {isLoading || isError ? (
-        <Skeleton
-          sx={{
-            backgroundImage:
-              'linear-gradient(131.88deg, #a63ee73b 14.48%, #00eaff2d 83.43%)',
-            borderRadius: '1rem ',
-          }}
-          animation='wave'
-          variant='rounded'
-          height={'200px'}
-          width={'100%'}
-        />
-      ) : (
-        <Splide
-          options={{
-            arrows: false,
-            pagination: false,
-            autoplay: true,
-            interval: 4000,
-            rewind: true,
-            gap: '1rem',
-            easing: 'ease',
-            fixedWidth: '100%',
-            mediaQuery: 'min',
-            breakpoints: {
-              900: {
-                fixedWidth: '45%',
-              },
-              1500: {
-                fixedWidth: '25%',
-              },
-            },
-            start: 0,
-          }}
-        >
-          {bestsellers?.map((bestseller, idx) => (
-            <SplideSlide key={bestseller.link}>
-              <BestsellersItem
-                img={bestseller.img}
-                price={bestseller.price}
-                name={bestseller.name}
-                link={bestseller.link}
-                idx={idx}
-              />
-            </SplideSlide>
-          ))}
-        </Splide>
-      )}
-    </StyledBestsellers>
-  );
+	return (
+		<StyledBestsellers>
+			<h3>Bestsellers</h3>
+			{isLoading || isError ? (
+				<Skeleton
+					sx={{
+						backgroundImage:
+							'linear-gradient(131.88deg, #a63ee73b 14.48%, #00eaff2d 83.43%)',
+						borderRadius: '1rem ',
+					}}
+					animation='wave'
+					variant='rounded'
+					height={'200px'}
+					width={'100%'}
+				/>
+			) : (
+				<Splide
+					options={{
+						arrows: false,
+						pagination: false,
+						autoplay: true,
+						interval: 4000,
+						rewind: true,
+						gap: '1rem',
+						easing: 'ease',
+						fixedWidth: '100%',
+						mediaQuery: 'min',
+						breakpoints: {
+							900: {
+								fixedWidth: '45%',
+							},
+							1500: {
+								fixedWidth: '25%',
+							},
+						},
+						start: 0,
+					}}
+				>
+					{bestsellers?.map((bestseller, idx) => (
+						<StyledSplideSlide key={bestseller.link}>
+							<BestsellersItem
+								img={bestseller.img}
+								price={bestseller.price}
+								name={bestseller.name}
+								link={bestseller.link}
+								idx={idx}
+							/>
+						</StyledSplideSlide>
+					))}
+				</Splide>
+			)}
+		</StyledBestsellers>
+	);
 };
 
 export default Bestsellers;
--- client/src/components/Bestsellers/BestsellersItem.tsx
+++ client/src/components/Bestsellers/BestsellersItem.tsx
@@ -5,70 +5,75 @@ import { Link } from 'react-router-dom';
 import getGameIdFromUrl from '#/utils/getGameIdFromUrl';
 
 const StyledBestsellersItem = styled(Link)`
-  border-radius: 0.7rem;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 100%;
-  }
+	border-radius: 0.7rem;
+	overflow: hidden;
+	img {
+		width: 100%;
+		height: 100%;
+	}
 `;
 
 const StyledOverlay = styled.span`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  left: 50%;
-  transform: translateX(-50%);
-  background: linear-gradient(
-    0deg,
-    rgba(0, 0, 0, 0.4) 30%,
-    rgba(255, 255, 255, 0) 50%
-  );
-  border-radius: 1rem;
-  @media screen and (min-width: 900px) {
-  }
-  span {
-    display: flex;
-    width: 80%;
-    justify-content: space-between;
-    position: absolute;
-    bottom: 10%;
-    left: 5%;
-    color: white;
-    font-weight: ${({ theme }) => theme.fontWeights.semiBold};
-    font-size: clamp(0.7rem, 2vw, 1rem);
-  }
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	left: 50%;
+	transform: translateX(-50%);
+	background: linear-gradient(
+		0deg,
+		rgba(0, 0, 0, 0.4) 30%,
+		rgba(255, 255, 255, 0) 50%
+	);
+	border-radius: 1rem;
+	color: white;
+	span {
+		display: flex;
+		width: 80%;
+		justify-content: center;
+		position: absolute;
+		bottom: 15%;
+		left: 50%;
+		transform: translateX(-50%);
+		font-weight: ${({ theme }) => theme.fontWeights.semiBold};
+		font-size: clamp(0.7rem, 2vw, 0.9rem);
+		text-align: center;
+	}
+`;
+const StyledCount = styled.p`
+	color: white;
+	position: absolute;
+	bottom: 7%;
+	right: 5%;
+	font-size: clamp(0.7rem, 2vw, 0.9rem);
 `;
 
 const BestsellersItem = ({
-  img,
-  link,
-  name,
-  price,
-  idx,
+	img,
+	link,
+	name,
+	idx,
 }: Bestseller & { idx: number }) => {
-  const gameId = getGameIdFromUrl(link);
-  return (
-    <Card>
-      <StyledBestsellersItem to={link}>
-        <img
-          src={`https://cdn.cloudflare.steamstatic.com/steam/apps/${gameId}/header.jpg`}
-          onError={({ currentTarget }) => {
-            currentTarget.onerror = null;
-            currentTarget.src = `${img}`;
-          }}
-          alt={`${name} image`}
-        />
-        <StyledOverlay>
-          <span>
-            <p>{name}</p>
-            <p>{idx + 1} / 10</p>
-            <p>{price || '0zł'}</p>
-          </span>
-        </StyledOverlay>
-      </StyledBestsellersItem>
-    </Card>
-  );
+	const gameId = getGameIdFromUrl(link);
+	return (
+		<Card>
+			<StyledBestsellersItem to={link}>
+				<img
+					src={`https://cdn.cloudflare.steamstatic.com/steam/apps/${gameId}/header.jpg`}
+					onError={({ currentTarget }) => {
+						currentTarget.onerror = null;
+						currentTarget.src = `${img}`;
+					}}
+					alt={`${name} image`}
+				/>
+				<StyledOverlay>
+					<span>
+						<p>{name}</p>
+					</span>
+					<StyledCount>{idx + 1} / 10</StyledCount>
+				</StyledOverlay>
+			</StyledBestsellersItem>
+		</Card>
+	);
 };
 
 export default BestsellersItem;
