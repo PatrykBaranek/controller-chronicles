84a3b0130adabc481966e5daab2c35da56379991
mbartos <maciekbartos8@gmail.com>
2023-10-10 13:40:54 +0200

    Change auth ui

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,137 +1,110 @@
 import styled from 'styled-components';
 import BurgerMenu from '../UI/BurgerMenu';
 import logo from '#/assets/logo.svg';
-import avatar from '#/assets/avatar.svg';
 import Searchbar from './Searchbar';
 import useWindowWidth from '../../hooks/useWindowWidth';
 import isLocationSearchable from '../../utils/isLocationSearchable';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import Filter from './Filter/Filter';
 import isDesktopWidth from '../../utils/isDesktopWidth';
 import Nav from '../Nav/Nav';
-import AuthButton from './AuthButton';
-import { useIsAuthenticated } from 'react-auth-kit';
-
-type Prop = {
-  isAuthLocation:boolean
-}
 
 const StyledHeader = styled.header`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  padding: 1.31rem 2.18rem;
-  overflow: hidden;
-  @media screen and (min-width: 900px) {
-    padding-bottom: 0;
-  }
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+	padding: 1.31rem 2.18rem;
+	overflow: hidden;
+	@media screen and (min-width: 900px) {
+		padding-bottom: 0;
+	}
 `;
 const StyledTopSection = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  padding-bottom: 1rem;
-  height: 50%;
-  position: relative;
-  &::after {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    content: '';
-    height: 1px;
-    width: 100%;
-    background: linear-gradient(
-      270deg,
-      rgba(255, 255, 255, 0) 0%,
-      rgba(255, 255, 255, 0.3) 50.4%,
-      rgba(255, 255, 255, 0) 96.77%
-    );
-  }
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	grid-template-rows: 1fr;
+	align-items: center;
+	width: 100%;
+	padding-bottom: 1rem;
+	height: 50%;
+	position: relative;
+	&::after {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		content: '';
+		height: 1px;
+		width: 100%;
+		background: linear-gradient(
+			270deg,
+			rgba(255, 255, 255, 0) 0%,
+			rgba(255, 255, 255, 0.3) 50.4%,
+			rgba(255, 255, 255, 0) 96.77%
+		);
+	}
 `;
 const StyledCtaSection = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 1.2rem;
-  align-items: center;
-  width: 100%;
-  height: 50%;
-  position: relative;
-  padding-bottom: 1.2rem;
-  @media screen and (min-width: 900px) {
-    padding-block: 1.7rem;
-  }
-  &::before {
-    position: absolute;
-    bottom: 0;
-    left: 50%;
-    content: '';
-    transform: rotate(180deg) translateX(50%);
-    height: 50%;
-    width: 80%;
-    background: ${({ theme }) => theme.colors.activeGradient};
-  }
-  &::after {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    content: '';
-    height: 1px;
-    width: 100%;
-    background: linear-gradient(
-      270deg,
-      rgba(255, 255, 255, 0) 0%,
-      rgba(255, 255, 255, 0.3) 50.4%,
-      rgba(255, 255, 255, 0) 96.77%
-    );
-  }
+	display: flex;
+	justify-content: space-between;
+	margin-top: 1.2rem;
+	align-items: center;
+	width: 100%;
+	height: 50%;
+	position: relative;
+	padding-bottom: 1.2rem;
+	@media screen and (min-width: 900px) {
+		padding-block: 1.7rem;
+	}
+	&::before {
+		position: absolute;
+		bottom: 0;
+		left: 50%;
+		content: '';
+		transform: rotate(180deg) translateX(50%);
+		height: 50%;
+		width: 80%;
+		background: ${({ theme }) => theme.colors.activeGradient};
+	}
+	&::after {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		content: '';
+		height: 1px;
+		width: 100%;
+		background: linear-gradient(
+			270deg,
+			rgba(255, 255, 255, 0) 0%,
+			rgba(255, 255, 255, 0.3) 50.4%,
+			rgba(255, 255, 255, 0) 96.77%
+		);
+	}
 `;
 const StyledLogo = styled.img`
-  width: clamp(10.6rem, 10vw, 18.75rem);
+	width: clamp(10.6rem, 10vw, 18.75rem);
 `;
-const StyledAuthWrapper = styled.div<Prop>`
-  display:${({isAuthLocation}) => isAuthLocation ? 'none':'flex'};
-  align-items: center;
-  gap:2rem;
-`
+
 const Header = () => {
-  const windowWidth = useWindowWidth();
-  const { pathname: location } = useLocation();
-  const isAuthenticated = useIsAuthenticated();
-  const isAuthLocation = location === '/login' || location === '/signup'
-  return (
-    <StyledHeader>
-      {!isDesktopWidth(windowWidth) && <Nav />}
-      <StyledTopSection>
-        <BurgerMenu />
-        <StyledLogo
-          src={logo}
-          alt='Controller chronicles logo'
-        />
-        {isDesktopWidth(windowWidth) && isLocationSearchable(location) && (
-          <Searchbar />
-        )}
-        <StyledAuthWrapper isAuthLocation={isAuthLocation}>
-          {isAuthenticated() && 
-          <Link to={'/auth/profile'}>
-            <img
-              src={avatar}
-              alt='User avatar'
-              />
-          </Link>
-          }
-          <AuthButton isAuth={isAuthenticated()}/>
-        </StyledAuthWrapper>
-      </StyledTopSection>
-      {isLocationSearchable(location) && (
-        <StyledCtaSection>
-          {!isDesktopWidth(windowWidth) && <Searchbar />}
-          <Filter />
-        </StyledCtaSection>
-      )}
-    </StyledHeader>
-  );
+	const windowWidth = useWindowWidth();
+	const { pathname: location } = useLocation();
+
+	return (
+		<StyledHeader>
+			{!isDesktopWidth(windowWidth) && <Nav />}
+			<StyledTopSection>
+				<BurgerMenu />
+				<StyledLogo src={logo} alt='Controller chronicles logo' />
+				{isDesktopWidth(windowWidth) && isLocationSearchable(location) && <Searchbar />}
+			</StyledTopSection>
+			{isLocationSearchable(location) && (
+				<StyledCtaSection>
+					{!isDesktopWidth(windowWidth) && <Searchbar />}
+					<Filter />
+				</StyledCtaSection>
+			)}
+		</StyledHeader>
+	);
 };
 
 export default Header;
--- client/src/components/UI/NavbarLink.tsx
+++ client/src/components/UI/NavbarLink.tsx
@@ -1,183 +1,210 @@
 import useStore from '#/store/store';
+import { useSignOut } from 'react-auth-kit';
 import { NavLink, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
 type NavbarLink = {
-  text: string;
-  icon: string;
+	text: string;
+	icon: string;
+	isLogoutButton?: boolean;
 };
 
 type StyledLink = {
-  isActive: boolean;
+	isActive: boolean;
 };
 
 const StyledNavbarLink = styled.li<StyledLink>`
-  display: flex;
-  justify-content: center;
-  height: 3.5rem;
-  position: relative;
-  @media screen and (min-width: 900px) {
-    justify-content: flex-start;
-    padding-left: 1.25rem;
-  }
+	display: flex;
+	justify-content: center;
+	height: 3.5rem;
+	position: relative;
+	@media screen and (min-width: 900px) {
+		justify-content: flex-start;
+		padding-left: 1.25rem;
+	}
 
-  &:hover {
-    background: ${({ isActive }) =>
-      isActive
-        ? ''
-        : `linear-gradient(
+	&:hover {
+		background: ${({ isActive }) =>
+			isActive
+				? ''
+				: `linear-gradient(
       131.88deg,
       rgba(0, 235, 255, 0) 0,
       rgba(167, 62, 231, 0.15) 14.48%,
       rgba(0, 235, 255, 0) 70%
     );`};
-  }
-  &::before {
-    display: ${({ isActive }) => (isActive ? 'inline' : 'none')};
-    position: absolute;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    content: '';
-    height: 50%;
-    width: 80%;
-    background: ${({ theme }) => theme.colors.activeGradient};
-    pointer-events: none;
-  }
-  &:not(:last-of-type) div::after {
-    display: none;
-  }
+	}
+	&::before {
+		display: ${({ isActive }) => (isActive ? 'inline' : 'none')};
+		position: absolute;
+		top: 0;
+		left: 50%;
+		transform: translateX(-50%);
+		content: '';
+		height: 50%;
+		width: 80%;
+		background: ${({ theme }) => theme.colors.activeGradient};
+		pointer-events: none;
+	}
+	&:not(:last-of-type) div::after {
+		display: none;
+	}
 `;
 const StyledWrapper = styled.div`
-  display: flex;
-  width: 30%;
-  @media screen and (min-width: 900px) {
-    width: 100%;
-  }
-  &::before {
-    position: absolute;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    content: '';
-    height: 1px;
-    width: 90%;
-    background: linear-gradient(
-      270deg,
-      rgba(255, 255, 255, 0) 0%,
-      rgba(255, 255, 255, 0.1) 50.4%,
-      rgba(255, 255, 255, 0) 96.77%
-    );
-  }
-  &::after {
-    position: absolute;
-    bottom: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    content: '';
-    height: 1px;
-    width: 80%;
-    background: linear-gradient(
-      270deg,
-      rgba(255, 255, 255, 0) 0%,
-      rgba(255, 255, 255, 0.1) 50.4%,
-      rgba(255, 255, 255, 0) 96.77%
-    );
-  }
-  a {
-    display: inherit;
-    gap: 1rem;
-    justify-content: center;
-    align-items: center;
-    color: ${({ theme }) => theme.colors.primary};
-    font-size: 0.9rem;
-    font-weight: ${({ theme }) => theme.fontWeights.semiBold};
-    @media screen and (min-width: 900px) {
-      width: 100%;
-      justify-content: flex-start;
-    }
-    span {
-      width: 1.5rem;
-      height: 1.5rem;
-    }
-    img {
-      width: 1.5rem;
-      height: 1.5rem;
-      filter: invert(90%) sepia(13%) saturate(181%) hue-rotate(201deg)
-        brightness(104%) contrast(92%);
-      opacity: 0.6;
-    }
-  }
-  a.active {
-    color: white;
-    span {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 2rem;
-      height: 2rem;
-      border-radius: 100vh;
-      position: relative;
-      margin-left: -5px;
-      &::before {
-        content: '';
-        position: absolute;
-        inset: 0;
-        border-radius: 100vh;
-        padding: 1px;
-        background: ${({ theme }) => theme.colors.secondaryGradient};
-        mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
-        mask-composite: xor;
-        mask-composite: exclude;
-        pointer-events: none;
-      }
-    }
-    p {
-      margin-left: -3px;
-    }
-    img {
-      opacity: 1;
-      filter: invert(100%) sepia(4%) saturate(10%) hue-rotate(66deg)
-        brightness(103%) contrast(100%);
-    }
-    img[alt='Games icon'] {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-48%, -56%);
-    }
-  }
+	display: flex;
+	width: 30%;
+	@media screen and (min-width: 900px) {
+		width: 100%;
+	}
+	&::before {
+		position: absolute;
+		top: 0;
+		left: 50%;
+		transform: translateX(-50%);
+		content: '';
+		height: 1px;
+		width: 90%;
+		background: linear-gradient(
+			270deg,
+			rgba(255, 255, 255, 0) 0%,
+			rgba(255, 255, 255, 0.1) 50.4%,
+			rgba(255, 255, 255, 0) 96.77%
+		);
+	}
+	&::after {
+		position: absolute;
+		bottom: 0;
+		left: 50%;
+		transform: translateX(-50%);
+		content: '';
+		height: 1px;
+		width: 80%;
+		background: linear-gradient(
+			270deg,
+			rgba(255, 255, 255, 0) 0%,
+			rgba(255, 255, 255, 0.1) 50.4%,
+			rgba(255, 255, 255, 0) 96.77%
+		);
+	}
+	a {
+		display: inherit;
+		gap: 1rem;
+		justify-content: center;
+		align-items: center;
+		color: ${({ theme }) => theme.colors.primary};
+		font-size: 0.9rem;
+		font-weight: ${({ theme }) => theme.fontWeights.semiBold};
+		@media screen and (min-width: 900px) {
+			width: 100%;
+			justify-content: flex-start;
+		}
+		span {
+			width: 1.5rem;
+			height: 1.5rem;
+		}
+		img {
+			width: 1.5rem;
+			height: 1.5rem;
+			filter: invert(90%) sepia(13%) saturate(181%) hue-rotate(201deg) brightness(104%)
+				contrast(92%);
+			opacity: 0.6;
+		}
+		img[alt='Profile icon'] {
+			filter: none;
+		}
+	}
+	a.active {
+		color: white;
+		span {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			width: 2rem;
+			height: 2rem;
+			border-radius: 100vh;
+			position: relative;
+			margin-left: -5px;
+			&::before {
+				content: '';
+				position: absolute;
+				inset: 0;
+				border-radius: 100vh;
+				padding: 1px;
+				background: ${({ theme }) => theme.colors.secondaryGradient};
+				mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+				mask-composite: xor;
+				mask-composite: exclude;
+				pointer-events: none;
+			}
+		}
+		p {
+			margin-left: -3px;
+		}
+		img {
+			opacity: 1;
+			filter: invert(100%) sepia(4%) saturate(10%) hue-rotate(66deg) brightness(103%)
+				contrast(100%);
+		}
+		img[alt='Games icon'] {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-48%, -56%);
+		}
+		img[alt='Login icon'],
+		img[alt='Log out icon'] {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -56%);
+		}
+		img[alt='Profile icon'] {
+			filter: none;
+		}
+	}
 `;
-const NavbarLink = ({ text, icon }: NavbarLink) => {
-  const { pathname } = useLocation();
-  const { toggleMenuOpen } = useStore();
+const NavbarLink = ({ text, icon, isLogoutButton }: NavbarLink) => {
+	const { pathname } = useLocation();
+	const { toggleMenuOpen } = useStore();
+	const logout = useSignOut();
+	const isLinkActive = () => {
+		if (text.toLowerCase() === 'home') {
+			return pathname === '/';
+		} else {
+			return pathname === `/${text.toLocaleLowerCase()}`;
+		}
+	};
 
-  const isLinkActive = () => {
-    if (text.toLowerCase() === 'home') {
-      return pathname === '/';
-    } else {
-      return pathname === `/${text.toLocaleLowerCase()}`;
-    }
-  };
-  return (
-    <StyledNavbarLink isActive={isLinkActive()}>
-      <StyledWrapper>
-        <NavLink
-          onClick={toggleMenuOpen}
-          className={({ isActive }) => (isActive ? 'active' : '')}
-          to={`/${text.toLowerCase() === 'home' ? '' : text.toLowerCase()}`}
-        >
-          <span>
-            <img
-              src={icon}
-              alt={`${text} icon`}
-            />
-          </span>
-          <p>{text}</p>
-        </NavLink>
-      </StyledWrapper>
-    </StyledNavbarLink>
-  );
+	const onLinkClick = () => {
+		toggleMenuOpen();
+		if (isLogoutButton) {
+			logout();
+		}
+	};
+
+	const correctLocation = () => {
+		if (text.toLowerCase() === 'home' || text.toLowerCase() === 'logout') {
+			return '';
+		}
+		return text.toLowerCase();
+	};
+
+	return (
+		<StyledNavbarLink isActive={isLinkActive()}>
+			<StyledWrapper>
+				<NavLink
+					onClick={onLinkClick}
+					className={({ isActive }) => (isActive && !isLogoutButton ? 'active' : '')}
+					to={`/${correctLocation()}`}
+				>
+					<span>
+						<img src={icon} alt={`${text} icon`} />
+					</span>
+					<p>{text}</p>
+				</NavLink>
+			</StyledWrapper>
+		</StyledNavbarLink>
+	);
 };
 
 export default NavbarLink;
