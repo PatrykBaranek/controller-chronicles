160d14a12f20b2f7166f124066f95ece3801d597
mbartos <maciekbartos8@gmail.com>
2023-11-10 13:28:15 +0100

    Cleanup dates

--- client/src/components/NewReleases/NewReleases.tsx
+++ client/src/components/NewReleases/NewReleases.tsx
@@ -5,33 +5,28 @@ import { useQuery } from 'react-query';
 import { getNewReleasedGames } from '#/api/gamesApi';
 
 const StyledWrapper = styled.div`
-  width: 100%;
-  height: 65vw;
-  @media screen and (min-width: 900px) {
-    padding-top: 2rem;
-    height: unset;
-    padding-inline: 1rem;
-  }
+	width: 100%;
+	height: 65vw;
+	@media screen and (min-width: 900px) {
+		padding-top: 2rem;
+		height: unset;
+		padding-inline: 1rem;
+	}
 `;
 
 const NewReleases = () => {
-  const currentDate = new Date();
-  const {
-    data: games,
-    isLoading,
-    isError,
-  } = useQuery(['newReleases'], () => getNewReleasedGames(currentDate), {
-    staleTime: 24 * 60 * 60 * 1000,
-  });
-  return (
-    <StyledWrapper>
-      <Carousel
-        newReleases={games?.results}
-        isLoading={isLoading}
-        isError={isError}
-      />
-    </StyledWrapper>
-  );
+	const {
+		data: games,
+		isLoading,
+		isError,
+	} = useQuery(['newReleases'], () => getNewReleasedGames(), {
+		staleTime: 24 * 60 * 60 * 1000,
+	});
+	return (
+		<StyledWrapper>
+			<Carousel newReleases={games?.results} isLoading={isLoading} isError={isError} />
+		</StyledWrapper>
+	);
 };
 
 export default NewReleases;
