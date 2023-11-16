5da188d6ce460e44b32fa1aa4f61da4b27c5118e
mbartos <maciekbartos8@gmail.com>
2023-11-16 21:12:52 +0100

    Change iframe

--- client/src/components/UI/VideoSlider.tsx
+++ client/src/components/UI/VideoSlider.tsx
@@ -1,118 +1,125 @@
-import { getLastMonthYoutubeVideos } from '#/api/gamesApi';
-import useWindowWidth from '#/hooks/useWindowWidth';
-import isDesktopWidth from '#/utils/isDesktopWidth';
-import { Skeleton } from '@mui/material';
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css';
-import { useQuery } from 'react-query';
-import styled from 'styled-components';
-import VideoSliderItem from './VideoSliderItem';
+import { getLastMonthYoutubeVideos } from "#/api/gamesApi";
+import useWindowWidth from "#/hooks/useWindowWidth";
+import isDesktopWidth from "#/utils/isDesktopWidth";
+import { Skeleton } from "@mui/material";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import { useQuery } from "react-query";
+import styled from "styled-components";
+import VideoSliderItem from "./VideoSliderItem";
 
 type Props = {
-	variant: 'review' | 'trailer';
-	heading: string;
+  variant: "review" | "trailer";
+  heading: string;
 };
 
 const StyledVideoSlider = styled.div`
-	padding-inline: 1rem;
-	margin-top: 10vw;
-	margin-bottom: 1rem;
-	display: flex;
-	flex-direction: column;
-	position: relative;
-	text-align: center;
-	.splide {
-		position: unset;
-		&__arrows {
-			position: absolute;
-			top: 3%;
-			right: 1%;
-		}
-		&__arrow {
-			background: #ffffff26;
-			border-radius: 7px;
-			width: 1.5rem;
-			height: 1.5rem;
-			svg {
-				fill: #fff;
-				opacity: 0.9;
-			}
-			&--prev {
-				left: -94vw;
-			}
-		}
-	}
-	@media screen and (min-width: 900px) {
-		text-align: left;
-		margin-top: 5vw;
-		.splide__arrow--prev {
-			left: -5rem;
-		}
-	}
-	h3 {
-		font-size: 1.2rem;
-		font-weight: ${({ theme }) => theme.fontWeights.semiBold};
-		margin-bottom: 1rem;
-	}
+  padding-inline: 1rem;
+  margin-top: 10vw;
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  text-align: center;
+  .splide {
+    position: unset;
+    &__arrows {
+      position: absolute;
+      top: 3%;
+      right: 1%;
+    }
+    &__arrow {
+      background: #ffffff26;
+      border-radius: 7px;
+      width: 1.5rem;
+      height: 1.5rem;
+      svg {
+        fill: #fff;
+        opacity: 0.9;
+      }
+      &--prev {
+        left: -94vw;
+      }
+    }
+  }
+  @media screen and (min-width: 900px) {
+    text-align: left;
+    margin-top: 5vw;
+    .splide__arrow--prev {
+      left: -5rem;
+    }
+  }
+  h3 {
+    font-size: 1.2rem;
+    font-weight: ${({ theme }) => theme.fontWeights.semiBold};
+    margin-bottom: 1rem;
+  }
 `;
 const StyledSplideSlide = styled(SplideSlide)`
-	@media screen and (min-width: 900px) {
-		padding-block: 1rem;
-	}
+  @media screen and (min-width: 900px) {
+    padding-block: 1rem;
+  }
 `;
 const VideoSlider = ({ variant, heading }: Props) => {
-	const windowWidth = useWindowWidth();
-	const isDesktop = isDesktopWidth(windowWidth);
-	const { data, isLoading, isError, isFetched } = useQuery([variant], () =>
-		getLastMonthYoutubeVideos(variant)
-	);
+  const windowWidth = useWindowWidth();
+  const isDesktop = isDesktopWidth(windowWidth);
+  const { data, isLoading, isError, isFetched } = useQuery([variant], () =>
+    getLastMonthYoutubeVideos(variant)
+  );
+  console.log(data?.slice(0, 34));
 
-	return (
-		<StyledVideoSlider>
-			<h3>{heading}</h3>
-			{isLoading || isError ? (
-				<Skeleton
-					sx={{
-						backgroundImage: 'linear-gradient(131.88deg, #a63ee73b 14.48%, #00eaff2d 83.43%)',
-						borderRadius: '1rem ',
-					}}
-					animation='wave'
-					variant='rounded'
-					height={isDesktop ? '20vw' : '55vw'}
-					width={'100%'}
-				/>
-			) : (
-				<Splide
-					options={{
-						arrows: true,
-						pagination: false,
-						rewind: true,
-						gap: '1rem',
-						easing: 'ease',
-						perPage: 1,
-						drag: false,
-						fixedWidth: '100%',
-						mediaQuery: 'min',
-						breakpoints: {
-							900: {
-								fixedWidth: '45%',
-								padding: '1rem',
-								perPage: 2,
-							},
-						},
-						start: 0,
-					}}
-				>
-					{isFetched &&
-						data?.map(({ link }, idx) => (
-							<StyledSplideSlide key={idx}>
-								<VideoSliderItem isDesktop={isDesktop} link={link} />
-							</StyledSplideSlide>
-						))}
-				</Splide>
-			)}
-		</StyledVideoSlider>
-	);
+  return (
+    <StyledVideoSlider>
+      <h3>{heading}</h3>
+      {isLoading || isError ? (
+        <Skeleton
+          sx={{
+            backgroundImage:
+              "linear-gradient(131.88deg, #a63ee73b 14.48%, #00eaff2d 83.43%)",
+            borderRadius: "1rem ",
+          }}
+          animation="wave"
+          variant="rounded"
+          height={isDesktop ? "20vw" : "55vw"}
+          width={"100%"}
+        />
+      ) : (
+        <Splide
+          options={{
+            arrows: true,
+            pagination: false,
+            rewind: true,
+            gap: "1rem",
+            easing: "ease",
+            perPage: 1,
+            drag: false,
+            fixedWidth: "100%",
+            mediaQuery: "min",
+            breakpoints: {
+              900: {
+                fixedWidth: "45%",
+                padding: "1rem",
+                perPage: 2,
+              },
+            },
+            start: 0,
+          }}
+        >
+          {isFetched &&
+            data?.slice(0, 34).map(({ link, thumbnail, title }, idx) => (
+              <StyledSplideSlide key={idx}>
+                <VideoSliderItem
+                  isDesktop={isDesktop}
+                  link={link}
+                  title={title}
+                  thumbnail={thumbnail}
+                />
+              </StyledSplideSlide>
+            ))}
+        </Splide>
+      )}
+    </StyledVideoSlider>
+  );
 };
 
 export default VideoSlider;
--- client/src/components/UI/VideoSliderItem.tsx
+++ client/src/components/UI/VideoSliderItem.tsx
@@ -1,42 +1,54 @@
-import { Skeleton } from '@mui/material';
-import styled from 'styled-components';
+import { Skeleton } from "@mui/material";
+import styled from "styled-components";
 
 const StyledVideoSliderItem = styled.iframe`
-	border-radius: 0.7rem;
-	display: block;
-	width: 100%;
-	border: none;
-	height: 55vw;
-	@media screen and (min-width: 900px) {
-		height: 20vw;
-	}
+  border-radius: 0.7rem;
+  display: block;
+  width: 100%;
+  border: none;
+  height: 55vw;
+  @media screen and (min-width: 900px) {
+    height: 20vw;
+  }
 `;
 
-const VideoSliderItem = ({ link, isDesktop }: { link: string; isDesktop: boolean }) => {
-	const isLinkCorrect = link.includes('embed') && link.length !== 0;
-	return (
-		<>
-			{!isLinkCorrect ? (
-				<Skeleton
-					sx={{
-						backgroundImage: 'linear-gradient(131.88deg, #a63ee73b 14.48%, #00eaff2d 83.43%)',
-						borderRadius: '1rem ',
-					}}
-					animation='wave'
-					variant='rounded'
-					height={isDesktop ? '20vw' : '55vw'}
-					width={'100%'}
-				/>
-			) : (
-				<StyledVideoSliderItem
-					src={link}
-					sandbox='allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation allow-presentation'
-					allowFullScreen
-					allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-				></StyledVideoSliderItem>
-			)}
-		</>
-	);
+const VideoSliderItem = ({
+  link,
+  isDesktop,
+  thumbnail,
+  title,
+}: {
+  link: string;
+  isDesktop: boolean;
+  thumbnail: string;
+  title: string;
+}) => {
+  const isLinkCorrect = link.includes("embed") && link.length !== 0;
+  return (
+    <>
+      {!isLinkCorrect ? (
+        <Skeleton
+          sx={{
+            backgroundImage:
+              "linear-gradient(131.88deg, #a63ee73b 14.48%, #00eaff2d 83.43%)",
+            borderRadius: "1rem ",
+          }}
+          animation="wave"
+          variant="rounded"
+          height={isDesktop ? "20vw" : "55vw"}
+          width={"100%"}
+        />
+      ) : (
+        <StyledVideoSliderItem
+          src={`${link}?autoplay=1`}
+          sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation allow-presentation"
+          allowFullScreen
+          srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=${link}?autoplay=1><img src=${thumbnail} alt=${title}><span>▶</span></a>`}
+          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; autoplay"
+        ></StyledVideoSliderItem>
+      )}
+    </>
+  );
 };
 
 export default VideoSliderItem;
