46dc82212a8c111923f73492199886fdd300b957
mbartos <maciekbartos8@gmail.com>
2023-11-16 21:12:52 +0100

    Change iframe

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,22 +1,22 @@
-import Bestsellers from '#/components/Bestsellers/Bestsellers';
-import NewReleases from '#/components/NewReleases/NewReleases';
-import VideoSlider from '#/components/UI/VideoSlider';
-import styled from 'styled-components';
+import Bestsellers from "#/components/Bestsellers/Bestsellers";
+import NewReleases from "#/components/NewReleases/NewReleases";
+import VideoSlider from "#/components/UI/VideoSlider";
+import styled from "styled-components";
 
 const StyledHome = styled.div`
-	width: 100%;
-	min-height: 100vh;
+  width: 100%;
+  min-height: 100vh;
 `;
 
 const Home = () => {
-	return (
-		<StyledHome>
-			<NewReleases />
-			<Bestsellers />
-			<VideoSlider variant='review' heading='Reviews' />
-			<VideoSlider variant='trailer' heading='Trailers' />
-		</StyledHome>
-	);
+  return (
+    <StyledHome>
+      <NewReleases />
+      <Bestsellers />
+      <VideoSlider variant="review" heading="Reviews" />
+      <VideoSlider variant="trailer" heading="Trailers" />
+    </StyledHome>
+  );
 };
 
 export default Home;
