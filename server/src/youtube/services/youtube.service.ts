56f05b415b2d131340de32a7beb34dafd51d849f
patrykbaranek <64849496+PatrykBaranek@users.noreply.github.com>
2023-11-21 21:04:34 +0100

    remove fetch by channel id

--- server/src/youtube/services/youtube.service.ts
+++ server/src/youtube/services/youtube.service.ts
@@ -8,7 +8,7 @@ import { GamesRepository }         from 'src/games/database/games.repository';
 import { GetGameVideoReviewDto }   from '../dto/get-game-video-review.dto';
 import { GetVideosByDateRangeDto } from '../dto/get-videos-by-date-range.dto';
 
-import { VideoType, REVIEW_CHANNEL_IDS, YoutubeUtilityService } from '../util/youtube-utility.service';
+import { VideoType, YoutubeUtilityService } from '../util/youtube-utility.service';
 
 const DAY_DIFFERENCE_THRESHOLD = 7;
 
@@ -33,28 +33,15 @@ export class YoutubeService {
   }
 
   async getVideosByDateRange(getVideosByDateRangeDto: GetVideosByDateRangeDto): Promise<SearchResultDto[]> {
-    const { fromDate, toDate, videoType, reviewChannels } = getVideosByDateRangeDto;
+    const { fromDate, toDate, videoType } = getVideosByDateRangeDto;
 
     const dateQuery = `${fromDate},${toDate}`;
 
     const games = await this.gamesService.getGames({ page: 1, page_size: 5, dates: dateQuery });
 
     const videos = await Promise.all(games.results.map(async (game) => {
-      if (reviewChannels && videoType === VideoType.REVIEW) {
-        return await this.getGameVideoReviewByGameIdByAllChannels(game.id);
-      } else {
-        return await this.getOrFetchGameVideos(game.id, videoType);
-      }
+      return await this.getOrFetchGameVideos(game.id, videoType);
     }));
-
-    return videos.flat();
-  }
-
-  private async getGameVideoReviewByGameIdByAllChannels(id: number): Promise<SearchResultDto[]> {
-    const allChannelIds = Object.values(REVIEW_CHANNEL_IDS);
-    const videos = await Promise.all(allChannelIds.map(channelId =>
-      this.getOrFetchGameVideos(id, VideoType.REVIEW, { channelId })
-    ));
 
     return videos.flat();
   }
@@ -67,6 +54,7 @@ export class YoutubeService {
     const dayDifference = differenceInDays(new Date(), gameInDb.updatedAt);
 
     if (gameInDb[videoFieldName] && dayDifference <= DAY_DIFFERENCE_THRESHOLD) {
+      this.logger.log(`Using cached ${videoType} for game ${gameInDb.rawgGame.name}`);
       return gameInDb[videoFieldName];
     }
 
