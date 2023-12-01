7e861556cfc607d82fdc82479703a757d924eb4e
mbartos <maciekbartos8@gmail.com>
2023-12-01 15:32:17 +0100

    Update utils

--- client/src/utils/isLocationSearchable.ts
+++ client/src/utils/isLocationSearchable.ts
@@ -1,5 +1,8 @@
 const isLocationSearchable = (location: string): boolean => {
-  return location.slice(0, 6) === '/games' || location.slice(0, 9) === '/podcasts';
+  return (
+    (location.slice(0, 6) === '/games' && location.slice(6, -1) === '') ||
+    (location.slice(0, 9) === '/podcasts' && location.slice(6, -1) === '')
+  );
 };
 
 export default isLocationSearchable;
