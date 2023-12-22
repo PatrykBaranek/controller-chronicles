1a454f7c5203f141073ae41f563f4ec602e0c7bd
mbartos <maciekbartos8@gmail.com>
2023-12-22 13:04:31 +0100

    Change private route logic

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,7 +1,9 @@
-import { RequireAuth } from 'react-auth-kit';
+import { useIsAuthenticated } from 'react-auth-kit';
+import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({ component }: { component: JSX.Element }) => {
-  return <RequireAuth loginPath='/login'>{component}</RequireAuth>;
+const PrivateRoute = ({ Component }: { Component: React.ComponentType<any> }) => {
+  const isAuthenticated = useIsAuthenticated();
+  return isAuthenticated() ? <Component /> : <Navigate to={'/login'} />;
 };
 
 export default PrivateRoute;
