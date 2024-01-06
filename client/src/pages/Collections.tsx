0e6c848ddfe37e605b40bd4fa8fb8f53de1ecc4b
mbartos <maciekbartos8@gmail.com>
2024-01-06 16:58:45 +0100

    Add adding collections

--- client/src/pages/Collections.tsx
+++ client/src/pages/Collections.tsx
@@ -1,13 +1,21 @@
 import { deleteCollection, getUserCollections } from '#/api/gamesApi';
+import leaveIcon from '#/assets/leaveIcon.svg';
+import trashIcon from '#/assets/trashIco.svg';
+import CollectionsForm from '#/components/Collections/CollectionsForm';
 import Card from '#/components/UI/Card';
 import getAuthToken from '#/utils/getAuthToken';
 import { Skeleton } from '@mui/material';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
-import { useQuery, useMutation } from 'react-query';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useMutation, useQuery } from 'react-query';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import leaveIcon from '#/assets/leaveIcon.svg';
-import trashIcon from '#/assets/trashIco.svg';
+import { StyledButton } from './Login';
+
+type StyledProps = {
+  hasCollections?: boolean;
+  hasGames?: boolean;
+};
 
 const StyledWrapper = styled.div`
   display: grid;
@@ -32,6 +40,7 @@ const StyledSplideSlide = styled(SplideSlide)`
     padding-block: 1rem;
   }
 `;
+
 const StyledCollectionItem = styled(Link)`
   border-radius: 0.7rem;
   overflow: hidden;
@@ -77,15 +86,31 @@ const StyledCollection = styled.div`
   }
 `;
 
+const StyledCollectionButton = styled(StyledButton)<StyledProps>`
+  width: fit-content;
+  font-weight: ${({ theme }) => theme.fontWeights.medium};
+  font-size: 1rem;
+  color: ${({ theme }) => theme.colors.secondary};
+  place-self: center;
+  margin-bottom: 1rem;
+  margin-block: ${({ hasGames }) => (!hasGames ? '2rem' : '')};
+  margin-inline: ${({ hasGames }) => (!hasGames ? 'auto' : '')};
+
+  @media screen and (min-width: 900px) {
+    place-self: ${({ hasCollections }) => (hasCollections ? 'end' : 'center')};
+    margin-right: 1rem;
+    margin-left: 0;
+  }
+`;
+
 const Collections = () => {
   const authToken = getAuthToken();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const navigate = useNavigate();
 
-  const {
-    data: collections,
-    isLoading,
-    isError,
-    refetch,
-  } = useQuery(['availableCollections'], () => getUserCollections(authToken));
+  const { data: collections, refetch } = useQuery(['availableCollections'], () =>
+    getUserCollections(authToken)
+  );
 
   const removeCollection = useMutation({
     mutationFn: (id: string) => deleteCollection(id, authToken),
@@ -99,75 +124,78 @@ const Collections = () => {
     });
   };
 
+  const toggleDialog = () => {
+    setIsDialogOpen((prev) => !prev);
+  };
+
   return (
     <StyledWrapper>
+      <StyledCollectionButton
+        hasGames={!!collections?.length}
+        hasCollections={!!collections?.length}
+        onClick={toggleDialog}
+      >
+        Add new collection
+      </StyledCollectionButton>
       {collections?.map((collection) => (
-        <>
-          {isLoading || isError ? (
-            <Skeleton
-              sx={{
-                backgroundImage: 'linear-gradient(131.88deg, #a63ee73b 14.48%, #00eaff2d 83.43%)',
-                borderRadius: '1rem',
-              }}
-              animation='wave'
-              variant='rounded'
-              height={'200px'}
-              width={'100%'}
-            />
-          ) : (
-            <StyledCollection>
-              <StyledCollectionTitle>
-                <Link className='heading' to={`${collection._id}`}>
-                  <h3>{collection.name}</h3>
-                  <img src={leaveIcon} alt='Leave icon' />
-                </Link>
-                <button onClick={() => handleDeleteCollection(collection._id)} className='delete'>
-                  <img src={trashIcon} alt='bin icon' />
-                </button>
-              </StyledCollectionTitle>
-              <Splide
-                key={`${collection.name}:${collection._id}`}
-                options={{
-                  arrows: false,
-                  pagination: false,
-                  autoplay: false,
-                  interval: 4000,
-                  rewind: true,
-                  gap: '1rem',
-                  easing: 'ease',
-                  perPage: 1,
-                  fixedWidth: '100%',
-                  mediaQuery: 'min',
-                  breakpoints: {
-                    900: {
-                      fixedWidth: '30%',
-                      padding: '1rem',
-                    },
-                    1500: {
-                      fixedWidth: '30%',
-                      perPage: 3,
-                    },
-                  },
-                  start: 0,
-                }}
-              >
-                {collection.games.map((game) => (
-                  <StyledSplideSlide key={`${collection.name}:${game._id}`}>
-                    <Card>
-                      <StyledCollectionItem to={`/games/${game._id}`}>
-                        <img
-                          src={game.rawgGame.background_image}
-                          alt={`${game.rawgGame.name} image`}
-                        />
-                      </StyledCollectionItem>
-                    </Card>
-                  </StyledSplideSlide>
-                ))}
-              </Splide>
-            </StyledCollection>
-          )}
-        </>
+        <StyledCollection key={collection._id}>
+          <StyledCollectionTitle>
+            <Link className='heading' to={`${collection._id}`}>
+              <h3>{collection.name}</h3>
+              <img src={leaveIcon} alt='Leave icon' />
+            </Link>
+            <button onClick={() => handleDeleteCollection(collection._id)} className='delete'>
+              <img src={trashIcon} alt='bin icon' />
+            </button>
+          </StyledCollectionTitle>
+          <Splide
+            key={`${collection.name}:${collection._id}`}
+            options={{
+              arrows: false,
+              pagination: false,
+              autoplay: false,
+              interval: 4000,
+              rewind: true,
+              gap: '1rem',
+              easing: 'ease',
+              perPage: 1,
+              fixedWidth: '100%',
+              mediaQuery: 'min',
+              breakpoints: {
+                900: {
+                  fixedWidth: '30%',
+                  padding: '1rem',
+                },
+                1500: {
+                  fixedWidth: '30%',
+                  perPage: 3,
+                },
+              },
+              start: 0,
+            }}
+          >
+            {!!collection.games.length ? (
+              collection.games.map((game) => (
+                <StyledSplideSlide key={`${collection.name}:${game._id}`}>
+                  <Card>
+                    <StyledCollectionItem to={`/games/${game._id}`}>
+                      <img
+                        src={game.rawgGame.background_image}
+                        alt={`${game.rawgGame.name} image`}
+                      />
+                    </StyledCollectionItem>
+                  </Card>
+                </StyledSplideSlide>
+              ))
+            ) : (
+              <StyledCollectionButton hasGames={false} onClick={() => navigate('/games')}>
+                Add game to collection
+              </StyledCollectionButton>
+            )}
+          </Splide>
+        </StyledCollection>
       ))}
+      <CollectionsForm handleClose={toggleDialog} isOpen={isDialogOpen} refetch={refetch} />
     </StyledWrapper>
   );
 };
