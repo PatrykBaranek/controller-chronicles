07a72387d2588e0f96cef1314c6f652a83860cec
patrykbaranek <64849496+PatrykBaranek@users.noreply.github.com>
2024-01-13 16:42:23 +0100

    add description_raw in /games response

--- server/src/games-update/games-update.module.ts
+++ server/src/games-update/games-update.module.ts
@@ -3,7 +3,6 @@ import { Module, forwardRef } from '@nestjs/common';
 import { GamesModule } from 'src/games/games.module';
 import { HowLongToBeatModule } from 'src/how-long-to-beat/how-long-to-beat.module';
 import { GamesUpdateService } from './services/games-update.service';
-import { ReviewsSitesModule } from 'src/reviews-sites/reviews-sites.module';
 import { RawgApiModule } from 'src/rawg/rawg-api/rawg-api.module';
 import { UpdateStrategyFactory } from './services/strategies/update-strategy-factory';
 
@@ -11,7 +10,6 @@ import { UpdateStrategyFactory } from './services/strategies/update-strategy-fac
   imports: [
     forwardRef(() => GamesModule),
     HowLongToBeatModule,
-    ReviewsSitesModule,
     RawgApiModule,
   ],
   providers: [GamesUpdateService, UpdateStrategyFactory],
