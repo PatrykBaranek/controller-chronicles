f8aed0352fe491619f08d86c67df84419efe5472
mbartos <maciekbartos8@gmail.com>
2024-01-27 17:12:36 +0100

    Add new types

--- client/src/types/types.d.ts
+++ client/src/types/types.d.ts
@@ -81,6 +81,31 @@ export type Episode = {
   release_date: string;
   release_date_precision: string;
   uri: string;
+  show: Show;
+};
+
+export type Show = {
+  available_markets: string[];
+  copyrights: [];
+  description: string;
+  explicit: boolean;
+  external_urls: Record<string, string>;
+  href: string;
+  html_description: string;
+  id: string;
+  images: {
+    height: number;
+    url: string;
+    width: number;
+  }[];
+  is_externally_hosted: boolean;
+  languages: string[];
+  media_type: string;
+  name: string;
+  publisher: string;
+  total_episodes: number;
+  type: string;
+  uri: string;
 };
 
 export type CollectionResponse = {
