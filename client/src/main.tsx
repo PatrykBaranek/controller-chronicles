f04d0a53aac47b94ad3475bc3049f069e1a0f360
Patryk Baranek <cien2460045@gmail.com>
2026-06-21 03:35:26 +0200

    fix: remove duplicate Header/Toaster from main.tsx outside router context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Theme from './Theme';
-import Header from './components/Header/Header';
-import { Toaster } from 'sonner';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Theme>
-      <Header />
       <App />
-      <Toaster />
     </Theme>
   </React.StrictMode>,
 );
